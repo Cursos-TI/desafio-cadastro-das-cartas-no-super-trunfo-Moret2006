afccae41946b810de3f37936ec24554453685db8
Moret2006 <200856686+Moret2006@users.noreply.github.com>
2025-03-18 20:35:09 +0000

    final

--- #include <stdio.c
+++ #include <stdio.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 
-int main() {
-/*
-operadores matematematicos
-soma = +
-subtracao = -
-multiplicacao = *
-divisao = /
-*/
+int main (){
 
-    int n1, n2;
-    int soma, subtracao, multiplicacao;
-    float divisao;
-    printf("Digite um numero:\n");
-    scanf("%d", &n1);
+    float temperatura, umidade;
+    unsigned int estoque, estoquemin = 1000;
+    
+    printf("entre com a temperatur\n");
+    scanf("%f", &temperatura);
 
-    printf("Digite o segundo numero:\n");
-    scanf("%d", &n2);
+    printf("informe a umidade\n");
+    scanf("%f", &umidade);
 
-    soma = n1 + n2;
-    subtracao = n1 - n2;
-    multiplicacao = n1 * n2;  
-    divisao = (float) n1 / n2; //foçando a variavel divisão sair em forma decimal
+    printf("informe a quantidade do seu estoque\n");
+    scanf("%d", &estoque);
+    
+    if (temperatura > 30)
+    printf("muito quente\n");
+    else
+    printf("temperatura normal\n");
 
-    //resultado
-    printf("O resultado é:\n");
-    printf("soma: %d \n", soma);
-    printf("subtracao: %d \n", subtracao);
-    printf("multipicacao: %d \n", multiplicacao);
-    printf("divisao: %.2f \n", divisao);
+    if (umidade > 50)
+    printf("umidade elevada\n");
+    else
+    printf("umidade normal\n");
+
+    if (estoque >= estoquemin)
+    printf("estoque ok\n");
+    else
+    printf("estoque baixo\n");
 }
